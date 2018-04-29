9f2d4abac4f6e2010a9f93c1d0588f7e4359d916
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-29 20:13:07 +0200

    add app

---
+++ server.js
@@ -0,0 +1,26 @@
+// [js] File is a CommonJS module; it may be converted to ES6 module - o co chodzi?
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+let stringifyFile = "";
+
+app.use(bodyParser.json());
+
+app.get('/getNote', (req, res) => {
+  fs.readFile('./test.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    stringifyFile = data;
+    res.send(data);
+  });
+});
+
+app.post('/updateNote/:note', (req, res) => {
+  stringifyFile += req.params.note;
+  fs.writeFile('./test.json', stringifyFile, (err) => {
+    if (err) throw err;
+    console.log('file updated');
+  });
+});
+
+app.listen(3000);
